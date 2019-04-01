2c260fc0cec5b040426c53d7b71275f91b860c5c
stravze <stravze@gmail.com>
2019-04-01 13:29:51 +0100

    fixing

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,6 @@
 terraform {
-  backend "azurerm" {
-  storage_account_name = "__bastion_sa__"
-    container_name       = "__bastion_name__"
-    key                  = "__bastion_name__/terraform.tfstate"
-	  access_key  = "__bastion_sk__"
-  }
+  required_version = ">= 0.11" 
+  backend "azurerm" {}
 }
 
 
