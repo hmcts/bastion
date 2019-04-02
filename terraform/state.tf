b3adc5b38be899d3eb7749e3b10e9fe3f80b9179
stravze <stravze@gmail.com>
2019-04-02 14:51:55 +0100

    secret

--- terraform/state.tf
+++
@@ -1,7 +0,0 @@
-#provider "azurerm" {
-#  version         = "1.21"
-#  tenant_id       = "${var.tenant_id}"
-#  subscription_id = "${var.subscription_id}"
-#  client_id       = "${var.client_id}"
-#  client_secret   = "${var.client_secret}"
-#}
