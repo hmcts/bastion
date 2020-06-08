69b8315c331383d853ddffc48e4bf010aaa94cd7
Gareth Heath <gareth.heath@hmcts.net>
2020-06-08 10:12:47 +0100

    pipeline development

--- terraform/component/sbox/20-bootstrap.tf
+++ terraform/component/sbox/20-bootstrap.tf
@@ -1,11 +1,11 @@
 module "bootstrap" {
   source              = "../../modules/bootstrap/"
   location            = "uksouth"
-  resource_group_name = "rdo-mgmt-bastion-sbox-rg"
-  vnet_name           = "rdo-mgmt-bastion-sbox-vnet"
+  resource_group_name = "bastion-sbox-rg"
+  vnet_name           = "bastion-sbox-vnet"
   vnet_address_space  = ["10.200.100.0/24"]
-  subnet_name         = "rdo-mgmt-bastion-sbox-subnet"
+  subnet_name         = "bastion-sbox-subnet"
   subnet_address      = "10.200.100.0/24"
-  keyvault_name       = "rdo-mgmt-bastion-sbox-kv"
+  keyvault_name       = "bastion-sbox-kv"
   environment         = "sbox"
 }
