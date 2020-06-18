211f0778c3df68dd2e768ce38de66c967198242c
Gareth Heath <gareth.heath@hmcts.net>
2020-06-18 10:12:07 +0100

    refactoring for rbac

--- terraform/environment/sbox/10-locals.tf
+++ terraform/environment/sbox/10-locals.tf
@@ -1,5 +1,5 @@
 locals {
-  environment  = "sbox"
-  location     = "uksouth"
-  subnet       = "10.20.20.96/27"
+  environment = "sbox"
+  location    = "uksouth"
+  subnet      = "10.20.20.96/27"
 }
--- terraform/modules/rbac/10-variables.tf
+++ terraform/modules/rbac/10-variables.tf
@@ -1,13 +1,3 @@
-variable "bastion_name" {
-  description = "Name of the bastion host"
-  type        = string
-}
-
-variable "role_def_assignable_scopes" {
-  description = "A list of subscriptions that the role definition will be assigned to"
-  type        = list(string)
-}
-
 variable "role_assignment_scope" {
   description = "The object ID of the bastion host, where roles will be assigned"
   type        = string
