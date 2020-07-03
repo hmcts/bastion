048151e8d03a02b912e487696904de9484ca7744
Gareth Heath <gareth.heath@hmcts.net>
2020-07-03 10:15:18 +0100

    move vnet child module to parent module

--- terraform/environment/nonprod/22-vnet-peering.tf
+++ terraform/environment/nonprod/22-vnet-peering.tf
@@ -1,9 +1,39 @@
-module "bastion-devops-peering" {
-  source                      = "../../modules/vnet_peering/"
-  bastion_vnet_name           = module.bootstrap.vnet.name
-  bastion_vnet_resource_group = module.bootstrap.vnet.resource_group_name
-  bastion_vnet_id             = module.bootstrap.vnet.id
-  hub_vnet_name               = "hmcts-dmz-nonprodi"
-  hub_vnet_resource_group     = "hmcts-hub-nonprodi"
-  hub_vnet_id                 = "caa9aa2e-7272-45ea-abec-4140678de647"
+provider "azurerm" {
+  version = ">= 2.0.0"
+  features {}
+}
+
+provider "azurerm" {
+  subscription_id = "fb084706-583f-4c9a-bdab-949aac66ba5c"
+  alias           = "hub-nonprod-intsvc"
+  features {}
+}
+
+data "azurerm_virtual_network" "hub" {
+  provider = azurerm.hub-nonprod-intsvc
+
+  name                = "hmcts-hub-nonprodi"
+  resource_group_name = "hmcts-hub-nonprodi"
+}
+
+resource "azurerm_virtual_network_peering" "bastion-to-hub" {
+  name                      = "${module.bootstrap.vnet.name}-to-hub"
+  resource_group_name       = module.bootstrap.vnet.resource_group_name
+  virtual_network_name      = module.bootstrap.vnet.name
+  remote_virtual_network_id = data.azurerm_virtual_network.hub.id
+
+  allow_virtual_network_access = "true"
+  allow_forwarded_traffic      = "true"
+}
+
+resource "azurerm_virtual_network_peering" "hub-to-bastion" {
+  provider = azurerm.hub-nonprod-intsvc
+
+  name                      = "hub-to-${module.bootstrap.vnet.name}"
+  resource_group_name       = data.azurerm_virtual_network.hub.resource_group_name
+  virtual_network_name      = data.azurerm_virtual_network.hub.name
+  remote_virtual_network_id = module.bootstrap.vnet.id
+
+  allow_virtual_network_access = "true"
+  allow_forwarded_traffic      = "true"
 }
