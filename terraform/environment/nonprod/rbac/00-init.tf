1f6e1d7404c26b9a8496096eb00e4a90ba493fc8
garethheath <36139793+garethheath@users.noreply.github.com>
2020-07-08 19:17:38 +0100

    nonprod redeployment (#9)
    * Multiple fixes

--- terraform/environment/nonprod/rbac/00-init.tf
+++ terraform/environment/nonprod/rbac/00-init.tf
@@ -1,9 +1,19 @@
 terraform {
   required_version = ">= 0.12.25"
-  backend "azurerm" {
-    subscription_id = "b44eb479-9ae2-42e7-9c63-f3c599719b6f"
-  }
+  # backend "azurerm" {
+  #   subscription_id = "b44eb479-9ae2-42e7-9c63-f3c599719b6f"
+  # }
   required_providers {
     azurerm = "~> 2.17.0"
   }
+}
+
+provider "azurerm" {
+  version = "2.17.0"
+  features {}
+}
+
+provider "azuread" {
+  version = "0.10"
+  features {}
 }
--- terraform/modules/vnet_peering/00-provider.tf
+++ terraform/modules/vnet_peering/00-provider.tf
@@ -2,6 +2,7 @@ provider "azurerm" {
   version = ">= 2.17.0"
   features {}
   skip_provider_registration = "true"
+  alias                      = "bastion"
 }
 
 provider "azurerm" {
