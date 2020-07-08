82968daea790293c5058216922b014adeeef255d
Gareth Heath <gareth.heath@hmcts.net>
2020-07-08 09:51:03 +0100

    add sub_id to tf init file

--- terraform/environment/nonprod/00-init.tf
+++ terraform/environment/nonprod/00-init.tf
@@ -1,6 +1,8 @@
 terraform {
   required_version = ">= 0.12.25"
-  backend "azurerm" {}
+  backend "azurerm" {
+    subscription_id = "b44eb479-9ae2-42e7-9c63-f3c599719b6f"
+  }
   required_providers {
     azurerm = "~> 2.0.0"
   }
