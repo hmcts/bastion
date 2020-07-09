331491a72f0680f0a375b34a136b00bf24eadc37
Gareth Heath <gareth.heath@hmcts.net>
2020-07-09 14:23:28 +0100

    fix type on data lookup

--- terraform/modules/vnet_peering/20-vnet-peering.tf
+++ terraform/modules/vnet_peering/20-vnet-peering.tf
@@ -1,7 +1,7 @@
 resource "azurerm_virtual_network_peering" "bastion-to-hub" {
   name                      = "${data.azurerm_virtual_network.bastion.name}-to-hub"
-  resource_group_name       = data.azurerm_virtual_machine.bastion.name
-  virtual_network_name      = data.azurerm_virtual_machine.bastion.resource_group_name
+  resource_group_name       = data.azurerm_virtual_network.bastion.resource_group_name
+  virtual_network_name      = data.azurerm_virtual_network.bastion.name
   remote_virtual_network_id = data.azurerm_virtual_network.hub.id
 
   allow_virtual_network_access = "true"
@@ -11,7 +11,7 @@ resource "azurerm_virtual_network_peering" "bastion-to-hub" {
 resource "azurerm_virtual_network_peering" "hub-to-bastion" {
   provider = azurerm.hub
 
-  name                      = "hub-to-${data.azurerm_virtual_machine.bastion.name}"
+  name                      = "hub-to-${data.azurerm_virtual_network.bastion.name}"
   resource_group_name       = data.azurerm_virtual_network.hub.resource_group_name
   virtual_network_name      = data.azurerm_virtual_network.hub.name
   remote_virtual_network_id = data.azurerm_virtual_network.bastion.id
