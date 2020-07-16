edb24f2685c09380697ee799ad0b57f89155da91
Gareth Heath <gareth.heath@hmcts.net>
2020-07-16 19:38:08 +0100

    set backend

--- terraform/environment/sbox/00-init.tf
+++ terraform/environment/sbox/00-init.tf
@@ -1,8 +1,8 @@
 terraform {
   required_version = ">= 0.12.25"
-  # backend "azurerm" {
-  #   subscription_id = "b3394340-6c9f-44ca-aa3e-9ff38bd1f9ac"
-  # }
+  backend "azurerm" {
+    subscription_id = "b3394340-6c9f-44ca-aa3e-9ff38bd1f9ac"
+  }
   required_providers {
     azurerm = "~> 2.17.0"
   }
