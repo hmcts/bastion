67d68d11f32b08bc4ffd560c37fce203a044a0cd
garethheath <36139793+garethheath@users.noreply.github.com>
2020-07-16 14:42:55 +0100

    Sbox bastion deployment (#15)
    * sbox deployment
    
    * reimplement pipeline trigger
    
    * add sbox deployment to azure-pipeline.yaml
    
    * fix storage accoutn name typo
    
    Co-authored-by: Gareth Heath <gareth.heath@hmcts.net>

--- terraform/environment/sbox/20-bootstrap.tf
+++ terraform/environment/sbox/20-bootstrap.tf
@@ -4,7 +4,7 @@ module "bootstrap" {
   environment         = local.environment
   resource_group_name = "bastion-${local.environment}-rg"
   vnet_name           = "bastion-${local.environment}-vnet"
-  keyvault_name       = "bastion-${local.environment}-kv"
+  keyvault_name       = "bastion-${local.environment}-hmcts-kv"
   subnet_name         = "bastion"
   subnet_address      = local.subnet
   vnet_address_space  = [local.subnet]
