c4e5c2a05c4f0ca61a019a4e6ae2a2c3d83de213
garethheath <36139793+garethheath@users.noreply.github.com>
2020-07-20 17:01:35 +0100

    add additional peerins for prod and nonprod (#18)
    Co-authored-by: Gareth Heath <gareth.heath@hmcts.net>

--- terraform/environment/nonprod/22-vnet-peering.tf
+++ terraform/environment/nonprod/22-vnet-peering.tf
@@ -6,3 +6,12 @@ module "bastion-devops-peering" {
   hub_vnet_resource_group     = "hmcts-hub-nonprodi"
   hub_subscription_id         = "fb084706-583f-4c9a-bdab-949aac66ba5c"
 }
+
+module "bastion-peering-to-vpn" {
+  source                      = "../../modules/vpn_peering/"
+  bastion_vnet_name           = module.bootstrap.vnet.name
+  bastion_vnet_resource_group = module.bootstrap.vnet.resource_group_name
+  vpn_vnet_name               = "core-infra-vnet-mgmt"
+  vpn_vnet_resource_group     = "rg-mgmt"
+  vpn_subscription_id         = "ed302caf-ec27-4c64-a05e-85731c3ce90e"
+}
