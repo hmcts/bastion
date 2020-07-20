3d91bbb9d8b28b96f4b4ded39d7d7d5781d1f521
Gareth Heath <gareth.heath@hmcts.net>
2020-07-20 09:54:27 +0100

    create vpn side of peering

--- terraform/modules/vpn_peering/20-vnet-peering.tf
+++ terraform/modules/vpn_peering/20-vnet-peering.tf
@@ -7,3 +7,15 @@ resource "azurerm_virtual_network_peering" "bastion-to-vpn" {
   allow_virtual_network_access = "true"
   allow_forwarded_traffic      = "true"
 }
+
+resource "azurerm_virtual_network_peering" "vpn-to-bastion" {
+  provider = azurerm.vpn
+
+  name                      = "vpn-to-${data.azurerm_virtual_network.bastion.name}"
+  resource_group_name       = data.azurerm_virtual_network.vpn.resource_group_name
+  virtual_network_name      = data.azurerm_virtual_network.vpn.name
+  remote_virtual_network_id = data.azurerm_virtual_network.bastion.id
+
+  allow_virtual_network_access = "true"
+  allow_forwarded_traffic      = "true"
+}
