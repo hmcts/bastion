4da14fe7e415ee52cc32c4cceafaf424e2b05a8f
Gareth Heath <gareth.heath@hmcts.net>
2020-07-21 15:10:45 +0100

    update routeing config for prod and nonprod

--- terraform/environment/prod/10-locals.tf
+++ terraform/environment/prod/10-locals.tf
@@ -1,5 +1,10 @@
 locals {
-  environment = "prod"
-  location    = "uksouth"
-  subnet      = "10.48.0.0/27"
+  environment             = "prod"
+  location                = "uksouth"
+  subnet                  = "10.48.0.0/27"
+  hub_subscription_id     = "0978315c-75fe-4ada-9d11-1eb5e0e0b214"
+  hub_vnet_name           = "hmcts-hub-prod-int"
+  hub_vnet_resource_group = "hmcts-hub-prod-int"
+  hub_lb_name             = "hmcts-hub-prod-int-palo-lb"
+  hub_lb_resource_group   = local.hub_vnet_resource_group
 }
