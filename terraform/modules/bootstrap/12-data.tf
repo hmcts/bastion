ef9015bb8e5f67aeb35f3b925c1b9bcd2ca3c22d
Gareth Heath <gareth.heath@hmcts.net>
2020-07-21 15:30:29 +0100

    fix provider typo

--- terraform/modules/bootstrap/12-data.tf
+++ terraform/modules/bootstrap/12-data.tf
@@ -1,7 +1,7 @@
 data "azurerm_client_config" "current" {}
 
 data "azurerm_lb" "hub_palo" {
-  provider = hub
+  provider = azurerm.hub
 
   name                = var.hub_lb_name
   resource_group_name = var.hub_lb_resource_group
