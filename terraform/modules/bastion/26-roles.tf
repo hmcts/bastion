338f0ad5593e0a8f924f7ac94637da86086e681b
Enda Kelly <enda.kelly@hmcts.net>
2021-08-20 15:02:43 +0100

    DTSPO-102 - removing user login role assignment

--- terraform/modules/bastion/26-roles.tf
+++ terraform/modules/bastion/26-roles.tf
@@ -2,12 +2,6 @@ data "azuread_group" "platform_operations" {
   display_name = "DTS Platform Operations"
 }
 
-resource "azurerm_role_assignment" "vm_user_login" {
-  role_definition_name = "Virtual Machine User Login"
-  scope                = azurerm_linux_virtual_machine.bastion.id
-  principal_id         = data.azuread_group.platform_operations.id
-}
-
 resource "azurerm_role_assignment" "vm_admin_login" {
   role_definition_name = "Virtual Machine Administrator Login"
   scope                = azurerm_linux_virtual_machine.bastion.id
