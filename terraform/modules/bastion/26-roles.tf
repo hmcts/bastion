97acc00d4b1c94b03475c823467104d352608868
Enda Kelly <enda.kelly@hmcts.net>
2021-08-23 11:31:41 +0100

    Removing roles.tf

--- terraform/modules/bastion/26-roles.tf
+++
@@ -1,9 +0,0 @@
-data "azuread_group" "platform_operations" {
-  display_name = "DTS Platform Operations"
-}
-
-resource "azurerm_role_assignment" "vm_admin_login" {
-  role_definition_name = "Virtual Machine Administrator Login"
-  scope                = azurerm_linux_virtual_machine.bastion.id
-  principal_id         = data.azuread_group.platform_operations.id
-}
