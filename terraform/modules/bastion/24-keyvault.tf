f981ab15966a4e925516d64c34d38997bb39d894
Enda Kelly <enda.kelly@hmcts.net>
2021-08-25 11:32:13 +0100

    attempting to fix syntax error

--- terraform/modules/bastion/24-keyvault.tf
+++ terraform/modules/bastion/24-keyvault.tf
@@ -18,7 +18,7 @@ resource "azurerm_key_vault_secret" "bastion_ssh_private_key" {
 
   lifecycle {
     ignore_changes = [
-      azurerm_key_vault_secret.bastion_ssh_private_key.value
+      value
     ]
   }
 }
@@ -30,7 +30,7 @@ resource "azurerm_key_vault_secret" "bastion_ssh_private_key_passphrase" {
 
   lifecycle {
     ignore_changes = [
-      azurerm_key_vault_secret.bastion_ssh_private_key_passphrase.value
+      value
     ]
   }
 }
