1e09d9d73b78886ed8a61e6bcfc52219bd39be1f
Enda Kelly <enda.kelly@hmcts.net>
2021-09-07 11:37:49 +0100

    Adding empty module to old bastions

--- terraform/environment/nonprod/21-bastion.tf
+++ terraform/environment/nonprod/21-bastion.tf
@@ -1,3 +1,14 @@
+module "bastion-dev" {
+  source              = "../../modules/empty-module/"
+  location            = module.bootstrap.resource_group.location
+  environment         = local.environment
+  image_id            = local.image_id
+  resource_group_name = module.bootstrap.resource_group.name
+  subnet_id           = module.bootstrap.subnet.id
+  keyvault_id         = module.bootstrap.keyvault.id
+  bastion_name        = "bastion-dev-nonprod"
+}
+
 module "bastion-nonprod" {
   source              = "../../modules/bastion/"
   location            = module.bootstrap.resource_group.location
--- terraform/environment/sbox/21-bastion.tf
+++ terraform/environment/sbox/21-bastion.tf
@@ -1,3 +1,14 @@
+module "bastion" {
+  source              = "../../modules/empty-module/"
+  location            = module.bootstrap.resource_group.location
+  environment         = local.environment
+  image_id            = local.image_id
+  resource_group_name = module.bootstrap.resource_group.name
+  subnet_id           = module.bootstrap.subnet.id
+  keyvault_id         = module.bootstrap.keyvault.id
+  bastion_name        = "bastion-dev-sbox"
+}
+
 module "bastion-sbox" {
   source              = "../../modules/bastion/"
   location            = module.bootstrap.resource_group.location
