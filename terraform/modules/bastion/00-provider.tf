4af92686af40ba87e71a20dab7890521e8f7730e
Enda Kelly <enda.kelly@hmcts.net>
2021-09-07 11:45:18 +0100

    Removing required providers block

--- terraform/modules/bastion/00-provider.tf
+++ terraform/modules/bastion/00-provider.tf
@@ -3,11 +3,3 @@ provider "azurerm" {
   features {}
   skip_provider_registration = "true"
 }
-
-terraform {
-  required_providers {
-    azurerm = {
-      source = "hashicorp/azurerm"
-    }
-  }
-}
