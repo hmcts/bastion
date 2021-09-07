c1c2aa11bda58c5849ff9ff5b0bb80888af4fdfe
Enda Kelly <enda.kelly@hmcts.net>
2021-09-07 11:29:48 +0100

    re-adding bastion devops

--- terraform/environment/prod/21-bastion.tf
+++ terraform/environment/prod/21-bastion.tf
@@ -1,3 +1,14 @@
+module "bastion-devops" {
+  source              = "../../modules/bastion/"
+  location            = module.bootstrap.resource_group.location
+  environment         = local.environment
+  image_id            = local.image_id
+  resource_group_name = module.bootstrap.resource_group.name
+  subnet_id           = module.bootstrap.subnet.id
+  keyvault_id         = module.bootstrap.keyvault.id
+  bastion_name        = "bastion-devops-prod"
+}
+
 module "bastion-prod" {
   source              = "../../modules/bastion/"
   location            = module.bootstrap.resource_group.location
