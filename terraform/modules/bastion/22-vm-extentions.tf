3a6d43b3b94b3fa75ca7b45340fd3c41a4b3a99c
TomKimber <thomas.kimber@hmcts.net>
2021-09-13 15:20:58 +0100

    Defaulted extensions config.

--- terraform/modules/bastion/22-vm-extentions.tf
+++ terraform/modules/bastion/22-vm-extentions.tf
@@ -22,11 +22,9 @@ module "splunk-uf" {
   count  = var.install_splunk_uf ? 1 : 0
   source = "git::https://github.com/hmcts/terraform-module-splunk-universal-forwarder.git?ref=master"
 
-  auto_upgrade_minor_version = false
-  virtual_machine_type       = "vm"
-  virtual_machine_id         = azurerm_linux_virtual_machine.bastion.id
-  splunk_username            = data.azurerm_key_vault_secret.splunk_username[0].value
-  splunk_password            = data.azurerm_key_vault_secret.splunk_password[0].value
-  splunk_pass4symmkey        = data.azurerm_key_vault_secret.splunk_pass4symmkey[0].value
-  type_handler_version       = "3.0"
+  virtual_machine_type = "vm"
+  virtual_machine_id   = azurerm_linux_virtual_machine.bastion.id
+  splunk_username      = data.azurerm_key_vault_secret.splunk_username[0].value
+  splunk_password      = data.azurerm_key_vault_secret.splunk_password[0].value
+  splunk_pass4symmkey  = data.azurerm_key_vault_secret.splunk_pass4symmkey[0].value
 }
