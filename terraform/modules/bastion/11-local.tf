9dd726ba64a880cd223e633bca7c195fb8ac83c1
TomKimber <thomas.kimber@hmcts.net>
2021-09-14 10:39:53 +0100

    Resolved issue with configure-bastion script.

--- terraform/modules/bastion/11-local.tf
+++ terraform/modules/bastion/11-local.tf
@@ -20,4 +20,6 @@ locals {
   splunk_username     = try(data.azurerm_key_vault_secret.splunk_username[0].value, false)
   splunk_password     = try(data.azurerm_key_vault_secret.splunk_password[0].value, false)
   splunk_pass4symmkey = try(data.azurerm_key_vault_secret.splunk_pass4symmkey[0].value, false)
+  cse_script          = "./configure-bastion.sh ${local.splunk_username} ${local.splunk_password} ${local.splunk_pass4symmkey}"
+  script_uri          = "https://raw.githubusercontent.com/hmcts/rdo-bastion/DTSPO-4817/terraform/modules/bastion/configure-bastion.sh?token=AALGNFWZHSOGDMXYJAFH54DBJGTEY"
 }
--- terraform/modules/bastion/22-vm-extentions.tf
+++ terraform/modules/bastion/22-vm-extentions.tf
@@ -8,23 +8,17 @@ resource "azurerm_virtual_machine_extension" "bastion_aad" {
 }
 
 resource "azurerm_virtual_machine_extension" "customscript" {
-  name                 = "formatdisks"
+  name                 = "configure-bastion"
   virtual_machine_id   = azurerm_linux_virtual_machine.bastion.id
   publisher            = "Microsoft.Azure.Extensions"
   type                 = "CustomScript"
   type_handler_version = "2.0"
-  protected_settings   = local.backend_config_json
+  protected_settings   = <<PROTECTED_SETTINGS
+    {
+      "fileUris": ["${local.script_uri}"],
+      "commandToExecute": "${local.cse_script}"
+    }
+    PROTECTED_SETTINGS
   depends_on           = [azurerm_virtual_machine_data_disk_attachment.diskattach]
 
-}
-
-module "splunk-uf" {
-  count  = var.install_splunk_uf ? 1 : 0
-  source = "git::https://github.com/hmcts/terraform-module-splunk-universal-forwarder.git?ref=master"
-
-  virtual_machine_type = "vm"
-  virtual_machine_id   = azurerm_linux_virtual_machine.bastion.id
-  splunk_username      = data.azurerm_key_vault_secret.splunk_username[0].value
-  splunk_password      = data.azurerm_key_vault_secret.splunk_password[0].value
-  splunk_pass4symmkey  = data.azurerm_key_vault_secret.splunk_pass4symmkey[0].value
 }
