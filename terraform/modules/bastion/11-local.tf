63b6a0af6570d3914838c8341beaadd966e294e6
TomKimber <thomas.kimber@hmcts.net>
2021-09-14 10:47:52 +0100

    Updated script_uri.

--- terraform/modules/bastion/11-local.tf
+++ terraform/modules/bastion/11-local.tf
@@ -20,5 +20,5 @@ locals {
   splunk_password     = try(data.azurerm_key_vault_secret.splunk_password[0].value, false)
   splunk_pass4symmkey = try(data.azurerm_key_vault_secret.splunk_pass4symmkey[0].value, false)
   cse_script          = "./configure-bastion.sh ${local.splunk_username} ${local.splunk_password} ${local.splunk_pass4symmkey}"
-  script_uri          = "https://raw.githubusercontent.com/hmcts/rdo-bastion/DTSPO-4817/terraform/modules/bastion/ConfigureBastion.sh?token=AALGNFWZHSOGDMXYJAFH54DBJGTEY"
+  script_uri          = "https://raw.githubusercontent.com/hmcts/rdo-bastion/DTSPO-4817/terraform/modules/bastion/ConfigureBastion.sh?token=AALGNFSWJQ2N645VEBP4QTTBJGVDO"
 }
