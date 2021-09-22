aa8606e1e01818466b492462984af11fdea094e6
Enda Kelly <enda.kelly@hmcts.net>
2021-09-22 13:03:56 +0100

    moving back to local

--- terraform/environment/sbox/21-bastion.tf
+++ terraform/environment/sbox/21-bastion.tf
@@ -2,7 +2,7 @@ module "bastion-sbox" {
   source              = "../../modules/bastion/"
   location            = module.bootstrap.resource_group.location
   environment         = local.environment
-  image_id            = "1.0.1"
+  image_id            = local.image_id
   resource_group_name = module.bootstrap.resource_group.name
   subnet_id           = module.bootstrap.subnet.id
   keyvault_id         = module.bootstrap.keyvault.id
