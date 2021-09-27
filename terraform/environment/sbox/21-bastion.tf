f93d291d64c7bce46407e96be771a2f4b3b66a20
Enda Kelly <enda.kelly@hmcts.net>
2021-09-27 12:09:59 +0100

    Extra image_id in sbox

--- terraform/environment/sbox/21-bastion.tf
+++ terraform/environment/sbox/21-bastion.tf
@@ -2,7 +2,7 @@ module "bastion-sbox" {
   source              = "../../modules/bastion/"
   location            = module.bootstrap.resource_group.location
   environment         = local.environment
-  image_id            = local.image_version
+  image_version            = local.image_version
   resource_group_name = module.bootstrap.resource_group.name
   subnet_id           = module.bootstrap.subnet.id
   keyvault_id         = module.bootstrap.keyvault.id
