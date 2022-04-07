3c717d258c9dbabf0fb947f537b01053f9c0909f
Tom Kimber <thomas.kimber@hmcts.net>
2022-04-07 11:01:30 +0100

    Update ConfigureBastion.sh

--- terraform/modules/bastion/ConfigureBastion.sh
+++ terraform/modules/bastion/ConfigureBastion.sh
@@ -21,6 +21,7 @@ curl --retry 3 -# -L -o $INSTALL_FILE $DOWNLOAD_URL
 tar xvzf $INSTALL_FILE -C $INSTALL_LOCATION
 rm -rf $INSTALL_FILE
 chown -R splunk:splunk $SPLUNK_HOME
+setfacl -R -m u:splunk:r /var/log
 
 if [  "$(systemctl is-active SplunkForwarder.service)" = "active"  ]; then
   $SPLUNK_HOME/bin/splunk stop
