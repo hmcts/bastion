db2d48551e253673728736a8ce2f3c94b8d9cf55
DeclanKainos <92787262+DeclanKainos@users.noreply.github.com>
2022-04-26 16:23:45 +0100

    ameneded bastion subscription id sbox

--- terraform/environment/sbox/rbac/20-rbac.tf
+++ terraform/environment/sbox/rbac/20-rbac.tf
@@ -4,7 +4,7 @@ module "bastion-secops-rbac" {
   bastion_resource_group          = "bastion-sbox-rg"
   bastion_access_admin_group_name = "DTS Non-Production Bastion Access for Administrators (SecOps)"
   bastion_access_user_group_name  = "DTS Non-Production Bastion Access for Users (SecOps)"
-  bastion_subscription_id         = "2b1afc19-5ca9-4796-a56f-574a58670244"
+  bastion_subscription_id         = "b3394340-6c9f-44ca-aa3e-9ff38bd1f9ac"
   aad_role_def_id_admin           = "/subscriptions/b3394340-6c9f-44ca-aa3e-9ff38bd1f9ac/providers/Microsoft.Authorization/roleDefinitions/1c0163c0-47e6-4577-8991-ea5c82e286e4"
   aad_role_def_id_user            = "/subscriptions/b3394340-6c9f-44ca-aa3e-9ff38bd1f9ac/providers/Microsoft.Authorization/roleDefinitions/fb879df8-f326-4884-b1cf-06f3ad86be52"
 }
