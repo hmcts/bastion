c790695cf8ec4b3138bfd3ebe508c1a45812643d
Tom Kimber <thomas.kimber@hmcts.net>
2022-05-04 13:39:33 +0100

    Removed Terraform templating.

--- terraform/modules/bastion/ConfigureBastion.sh
+++ terraform/modules/bastion/ConfigureBastion.sh
@@ -19,35 +19,35 @@ else
     # if data disks are present, get details
   lsblk -d -o name,size | tee -a /var/log/messages | grep -i sd | grep -v sda | grep -v sdb |  while read id size
   do
-      details=$(fdisk -l /dev/${id}| grep -i ^\/dev\/sd*)
+      details=$(fdisk -l /dev/$id| grep -i ^\/dev\/sd*)
 
       # if data disks are not already formatted, format and mount them
       if [[ -z $details ]]
       then
-        logger -s "custom_script:: Currently working on disk ${id}"
+        logger -s "custom_script:: Currently working on disk $id"
         parted /dev/$id --script mklabel gpt mkpart xfspart xfs 0% 100%
         sleep 120 # wait till filesystem is created
-        logger -s  "custom_script:: creating the FS on ${id}1"
-          mkfs.xfs /dev/${id}1
-        partprobe /dev/${id}1
+        logger -s  "custom_script:: creating the FS on $id1"
+          mkfs.xfs /dev/$id1
+        partprobe /dev/$id1
         if [[ $size =~ \d*G ]]
         then
-          logger -s "custom_script:: mounting /dev/${id}1 to /opt/data-migration"
+          logger -s "custom_script:: mounting /dev/$id1 to /opt/data-migration"
           if [[ !  -d /opt/data-migration ]]
             then
               mkdir /opt/data-migration
           fi
-          mount /dev/${id}1 /opt/data-migration
-          uuid=$(blkid -s UUID -o value /dev/${id}1)
+          mount /dev/$id1 /opt/data-migration
+          uuid=$(blkid -s UUID -o value /dev/$id1)
           sed -i.bak '\/opt\/data-migration/d' /etc/fstab
           echo "UUID=$uuid /opt/data-migration  xfs   defaults,nofail   1   2" >> /etc/fstab
-          logger -s "${id} disk is partitioned and mounted"
+          logger -s "$id disk is partitioned and mounted"
         fi
       else
 
         # if data disks are present and formatted but not mounted, mount them
-        mount /dev/${id}1 /opt/data-migration
-        uuid=$(blkid -s UUID -o value /dev/${id}1)
+        mount /dev/$id1 /opt/data-migration
+        uuid=$(blkid -s UUID -o value /dev/$id1)
         sed -i.bak '\/opt\/data-migration/d' /etc/fstab
         echo "UUID=$uuid /opt/data-migration  xfs   defaults,nofail   1   2" >> /etc/fstab
         logger -s "custom_script:: disk is already partitioned but not mounted...mounting now"
