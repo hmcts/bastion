cc9ae7fc6766fb50d2ac446c263e37b3d4d2d452
DeclanKainos <92787262+DeclanKainos@users.noreply.github.com>
2022-05-04 11:11:03 +0100

    temporarily commented out RBAC block

--- terraform/environment/sbox/rbac/20-rbac.tf
+++ terraform/environment/sbox/rbac/20-rbac.tf
@@ -1,3 +1,4 @@
+/*
 module "bastion-secops-sbox-rbac" {
   source = "../../../modules/rbac/"
   bastion_vm_name                 = "bastion-secops-sbox"
@@ -8,6 +9,7 @@ module "bastion-secops-sbox-rbac" {
   aad_role_def_id_admin           = "/subscriptions/b3394340-6c9f-44ca-aa3e-9ff38bd1f9ac/providers/Microsoft.Authorization/roleDefinitions/1c0163c0-47e6-4577-8991-ea5c82e286e4"
   aad_role_def_id_user            = "/subscriptions/b3394340-6c9f-44ca-aa3e-9ff38bd1f9ac/providers/Microsoft.Authorization/roleDefinitions/fb879df8-f326-4884-b1cf-06f3ad86be52"
 }
+*/
 module "bastion-sbox-rbac" {
   source                          = "../../../modules/rbac/"
   bastion_vm_name                 = "bastion-sbox"
