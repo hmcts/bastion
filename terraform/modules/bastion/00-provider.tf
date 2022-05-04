0ce0ae0fa9405a4701fe07fc8afcd8547df49bf2
Tom Kimber <thomas.kimber@hmcts.net>
2022-05-04 13:28:55 +0100

    Added skip_provider_registration = true.

--- terraform/modules/bastion/00-provider.tf
+++ terraform/modules/bastion/00-provider.tf
@@ -5,19 +5,22 @@ provider "azurerm" {
 }
 
 provider "azurerm" {
-  alias = "soc"
+  alias                      = "soc"
+  skip_provider_registration = "true"
   features {}
   subscription_id = "8ae5b3b6-0b12-4888-b894-4cec33c92292"
 }
 
 provider "azurerm" {
-  alias = "shared_image_gallery"
+  alias                      = "shared_image_gallery"
+  skip_provider_registration = "true"
   features {}
   subscription_id = "2b1afc19-5ca9-4796-a56f-574a58670244"
 }
 
 provider "azurerm" {
-  alias = "cnp"
+  alias                      = "cnp"
+  skip_provider_registration = "true"
   features {}
   subscription_id = var.cnp_vault_sub
 }
