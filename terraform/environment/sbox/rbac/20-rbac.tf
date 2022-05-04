46030870f674d289354139a34fb2118dbec6c519
DeclanKainos <92787262+DeclanKainos@users.noreply.github.com>
2022-05-04 11:49:42 +0100

    tf fmt

--- terraform/environment/sbox/rbac/20-rbac.tf
+++ terraform/environment/sbox/rbac/20-rbac.tf
@@ -1,6 +1,6 @@
 # RBAC settings for SecOps bastion
 module "bastion-secops-sbox-rbac" {
-  source = "../../../modules/rbac/"
+  source                          = "../../../modules/rbac/"
   bastion_vm_name                 = "bastion-secops-sbox"
   bastion_resource_group          = "bastion-sbox-rg"
   bastion_access_admin_group_name = "DTS Non-Production Bastion Access for Administrators (SecOps)"
