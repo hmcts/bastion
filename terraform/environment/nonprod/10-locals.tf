4575f2b0f8ddd1bdbeb9e2327aea2687f9f668ed
Brendon Higgins <brendon.higgins@kainos.com>
2022-08-03 17:55:55 +0100

    Back out subnet mask change
    Returned back to subnet  = "10.48.0.32/27"

--- terraform/environment/nonprod/10-locals.tf
+++ terraform/environment/nonprod/10-locals.tf
@@ -2,7 +2,7 @@ locals {
   environment             = "nonprod"
   location                = "uksouth"
   image_version           = "1.0.3"
-  subnet                  = "10.48.0.32/28"
+  subnet                  = "10.48.0.32/27"
   hub_subscription_id     = "fb084706-583f-4c9a-bdab-949aac66ba5c"
   hub_vnet_name           = "hmcts-hub-nonprodi"
   hub_vnet_resource_group = "hmcts-hub-nonprodi"
