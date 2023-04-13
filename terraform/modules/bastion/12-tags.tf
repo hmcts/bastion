7c4b1c7ece2a716c1bb1fa8eede8ee0351ecec56
felix.eyetan <felix.eyetan@justice.gov.uk>
2023-04-13 11:17:57 +0100

    Formatting code

--- terraform/modules/bastion/12-tags.tf
+++ terraform/modules/bastion/12-tags.tf
@@ -1,8 +1,8 @@
 module "ctags" {
   source = "github.com/hmcts/terraform-module-common-tags"
 
-  builtFrom   = "https://github.com/hmcts/bastion"
-  environment = var.environment
-  product     = "mgmt"
+  builtFrom    = "https://github.com/hmcts/bastion"
+  environment  = var.environment
+  product      = "mgmt"
   expiresAfter = "3000-01-01" # never expire bastions
 }
