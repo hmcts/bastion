a5a94a0d42b3aab5c2ffab56e27fbe544dc5cda9
felix.eyetan <felix.eyetan@justice.gov.uk>
2023-04-13 11:01:50 +0100

    update targrting sbox

--- terraform/modules/bastion/12-tags.tf
+++ terraform/modules/bastion/12-tags.tf
@@ -4,4 +4,5 @@ module "ctags" {
   builtFrom   = "https://github.com/hmcts/bastion"
   environment = var.environment
   product     = "mgmt"
+  expiresAfter = "3000-01-01" # never expire bastions
 }
