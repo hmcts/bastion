5fae3662b7f803f865e68bbd7a0c4ed57802dfb4
Tom Kimber <thomas.kimber@hmcts.net>
2023-09-07 14:36:51 +0100

    Update ConfigureBastion.sh

--- terraform/modules/bastion/ConfigureBastion.sh
+++ terraform/modules/bastion/ConfigureBastion.sh
@@ -27,18 +27,18 @@ else
         logger -s "custom_script:: Currently working on disk $id"
         parted /dev/$id --script mklabel gpt mkpart xfspart xfs 0% 100%
         sleep 120 # wait till filesystem is created
-        logger -s  "custom_script:: creating the FS on $id1"
-          mkfs.xfs /dev/$id1
-        partprobe /dev/$id1
+        logger -s  "custom_script:: creating the FS on $(id)1"
+          mkfs.xfs /dev/$(id)1
+        partprobe /dev/$(id)1
         if [[ $size =~ \d*G ]]
         then
-          logger -s "custom_script:: mounting /dev/$id1 to /opt/data-migration"
+          logger -s "custom_script:: mounting /dev/$(id)1 to /opt/data-migration"
           if [[ !  -d /opt/data-migration ]]
             then
               mkdir /opt/data-migration
           fi
-          mount /dev/$id1 /opt/data-migration
-          uuid=$(blkid -s UUID -o value /dev/$id1)
+          mount /dev/$(id)1 /opt/data-migration
+          uuid=$(blkid -s UUID -o value /dev/$(id)1)
           sed -i.bak '\/opt\/data-migration/d' /etc/fstab
           echo "UUID=$uuid /opt/data-migration  xfs   defaults,nofail   1   2" >> /etc/fstab
           logger -s "$id disk is partitioned and mounted"
@@ -46,8 +46,8 @@ else
       else
 
         # if data disks are present and formatted but not mounted, mount them
-        mount /dev/$id1 /opt/data-migration
-        uuid=$(blkid -s UUID -o value /dev/$id1)
+        mount /dev/$(id)1 /opt/data-migration
+        uuid=$(blkid -s UUID -o value /dev/$(id)1)
         sed -i.bak '\/opt\/data-migration/d' /etc/fstab
         echo "UUID=$uuid /opt/data-migration  xfs   defaults,nofail   1   2" >> /etc/fstab
         logger -s "custom_script:: disk is already partitioned but not mounted...mounting now"
