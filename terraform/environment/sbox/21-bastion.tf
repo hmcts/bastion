0d2fd51c29e47894d3d7bc2a68977c7bfea89963
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2024-04-03 16:30:42 +0100

    Update 21-bastion.tf

--- terraform/environment/sbox/21-bastion.tf
+++ terraform/environment/sbox/21-bastion.tf
@@ -14,7 +14,7 @@ module "bastion-sbox" {
   cnp_vault_rg        = "cnp-core-infra"
   cnp_vault_sub       = "1c4f0704-a29e-403d-b719-b90c34ef14c9"
   dynatrace_tenant_id = "yrk32651"
-  dynatrace_server    = "https://10.10.70.6:9999/e/yrk32651/api"
+  dynatrace_server    = "https://dynatrace-activegate-nonprod.platform.hmcts.net:9999/e/yrk32651/api"
   # Tenable Nessus
   nessus_server     = "nessus-scanners-sbox000006.platform.hmcts.net"
   nessus_key_secret = "nessus-agent-key-sbox"
@@ -34,7 +34,7 @@ module "bastion-secops-sbox" {
   cnp_vault_rg        = "cnp-core-infra"
   cnp_vault_sub       = "1c4f0704-a29e-403d-b719-b90c34ef14c9"
   dynatrace_tenant_id = "yrk32651"
-  dynatrace_server    = "https://10.10.70.6:9999/e/yrk32651/api"
+  dynatrace_server    = "https://dynatrace-activegate-nonprod.platform.hmcts.net:9999/e/yrk32651/api"
   # Tenable Nessus
   nessus_server     = "nessus-scanners-sbox000006.platform.hmcts.net"
   nessus_key_secret = "nessus-agent-key-sbox"
