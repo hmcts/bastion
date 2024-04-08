679a2d22184cb700aa3ec80bae3f778531a231dd
mananpatel26 <manan.patel@kainos.com>
2024-04-08 12:51:58 +0100

    Upgrading splunk usf version

--- terraform/environment/sbox/21-bastion.tf
+++ terraform/environment/sbox/21-bastion.tf
@@ -16,9 +16,9 @@ module "bastion-sbox" {
   dynatrace_tenant_id = "yrk32651"
   dynatrace_server    = "https://dynatrace-activegate-nonprod.platform.hmcts.net:9999/e/yrk32651/api"
   # Tenable Nessus
-  nessus_server     = "nessus-scanners-sbox000006.platform.hmcts.net"
-  nessus_key_secret = "nessus-agent-key-sbox"
-  vm_bootstrap_source     = "github.com/hmcts/terraform-module-vm-bootstrap?ref=DTSPO-17050-splunk-usf-version-update"
+  nessus_server       = "nessus-scanners-sbox000006.platform.hmcts.net"
+  nessus_key_secret   = "nessus-agent-key-sbox"
+  vm_bootstrap_source = "github.com/hmcts/terraform-module-vm-bootstrap?ref=DTSPO-17050-splunk-usf-version-update"
 
 }
 
