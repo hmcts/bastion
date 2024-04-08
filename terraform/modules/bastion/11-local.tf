cfb4521ddb372168e6978cd84b9adbf6486c2759
mananpatel26 <manan.patel@kainos.com>
2024-04-08 10:47:55 +0100

    Updating splunk usf version for sbox

--- terraform/modules/bastion/11-local.tf
+++ terraform/modules/bastion/11-local.tf
@@ -16,7 +16,7 @@ locals {
     "environment"        = local.env_display_names[var.environment]
   }
 
-  dynatrace_env = var.dynatrace_tenant_id == "yrk32651" ? "nonprod" : var.dynatrace_tenant_id == "ebe20728" ? "prod" : null
+  dynatrace_env       = var.dynatrace_tenant_id == "yrk32651" ? "nonprod" : var.dynatrace_tenant_id == "ebe20728" ? "prod" : null
   vm_bootstrap_source = var.environment == "sbox" ? "github.com/hmcts/terraform-module-vm-bootstrap?ref=DTSPO-17050-splunk-usf-version-update" : "github.com/hmcts/terraform-module-vm-bootstrap?ref=master"
 
 }
