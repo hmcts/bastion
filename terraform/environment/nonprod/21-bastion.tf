6380464fb317acfc67d8f666afa778b66cb4e7c9
Ciarán Carragher <ciaran.carragher2@justice.gov.uk>
2024-04-15 16:18:14 +0100

    Update nonprod dynatrace server url

--- terraform/environment/nonprod/21-bastion.tf
+++ terraform/environment/nonprod/21-bastion.tf
@@ -14,7 +14,7 @@ module "bastion-nonprod" {
   cnp_vault_rg        = "cnp-core-infra"
   cnp_vault_sub       = "1c4f0704-a29e-403d-b719-b90c34ef14c9"
   dynatrace_tenant_id = "yrk32651"
-  dynatrace_server    = "https://10.10.70.6:9999/e/yrk32651/api"
+  dynatrace_server    = "https://dynatrace-activegate-nonprod.platform.hmcts.net:9999/e/yrk32651/api"
   # Tenable Nessus
   nessus_server     = "nessus-scanners-nonprod000005.platform.hmcts.net"
   nessus_key_secret = "nessus-agent-key-nonprod"
