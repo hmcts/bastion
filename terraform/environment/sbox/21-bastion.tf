850dd23508f7ced7282173fde071d7e99361c6ec
nicholas chang <nicholasc001@hotmail.com>
2024-08-09 14:34:08 +0100

    output tag

--- terraform/environment/sbox/21-bastion.tf
+++ terraform/environment/sbox/21-bastion.tf
@@ -18,7 +18,7 @@ module "bastion-sbox" {
   # Tenable Nessus
   nessus_server     = "nessus-scanners-sbox000006.platform.hmcts.net"
   nessus_key_secret = "nessus-agent-key-sbox"
-  common_tags       = merge(module.ctags.common_tags, local.auto_shutdown_common_tags)
+  common_tags       = merge(module.bastion.common_tags, local.auto_shutdown_common_tags)
 }
 
 # Configuration for SecOps bastion
@@ -39,5 +39,5 @@ module "bastion-secops-sbox" {
   # Tenable Nessus
   nessus_server     = "nessus-scanners-sbox000006.platform.hmcts.net"
   nessus_key_secret = "nessus-agent-key-sbox"
-  common_tags       = merge(module.ctags.common_tags, local.auto_shutdown_common_tags)
+  common_tags       = merge(module.bastion.common_tags, local.auto_shutdown_common_tags)
 }
