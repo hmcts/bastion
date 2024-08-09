cf04eb8a9634a0585af0639019d55fc01c9ac900
nicholas chang <nicholasc001@hotmail.com>
2024-08-09 14:36:26 +0100

    remove output

--- terraform/modules/bastion/30-output.tf
+++ terraform/modules/bastion/30-output.tf
@@ -2,8 +2,3 @@ output "virtual_machine" {
   description = "The bastion virtual machine"
   value       = azurerm_linux_virtual_machine.bastion
 }
-
-output "common_tags" {
-  description = "Returns a mapping of tags to assign."
-  value = module.ctags.common_tags
-}
