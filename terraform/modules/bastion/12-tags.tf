4b498f4ad23cbf470c1bf1ab0d90bbfece79bd18
nicholas chang <nicholasc001@hotmail.com>
2024-08-09 14:48:00 +0100

    remove tag module to root module

--- terraform/modules/bastion/12-tags.tf
+++
@@ -1,8 +0,0 @@
-module "ctags" {
-  source = "github.com/hmcts/terraform-module-common-tags"
-
-  builtFrom    = "https://github.com/hmcts/bastion"
-  environment  = var.environment
-  product      = "mgmt"
-  expiresAfter = "3000-01-01" # never expire bastions
-}
