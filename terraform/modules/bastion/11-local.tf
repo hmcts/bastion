b9922fe655bf87a53f06b49a5766937636cb6876
nicholas chang <nicholasc001@hotmail.com>
2024-08-12 17:26:45 +0100

    format code

--- terraform/modules/bastion/11-local.tf
+++ terraform/modules/bastion/11-local.tf
@@ -19,9 +19,8 @@ locals {
   dynatrace_env = var.dynatrace_tenant_id == "yrk32651" ? "nonprod" : var.dynatrace_tenant_id == "ebe20728" ? "prod" : null
 
   include_in_autoshutdown = local.env_display_names[var.environment] == "Production" ? "false" : "true"
-  
-  merged_tags = local.env_display_names[var.environment] == "Production" ? {} : merge(module.ctags.common_tags, local.auto_shutdown_common_tags)
 
+  merged_tags = local.env_display_names[var.environment] == "Production" ? {} : merge(module.ctags.common_tags, local.auto_shutdown_common_tags)
 
   auto_shutdown_common_tags = {
     "startupMode"  = "always",
